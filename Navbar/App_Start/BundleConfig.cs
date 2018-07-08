472bedbd93979ce0f489b0e6eb3773ea40a4535b
Goyza <goyza666@gmail.com>
2018-07-08 08:39:08 +0200

    fix2

--- Navbar/App_Start/BundleConfig.cs
+++ Navbar/App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace Navbar
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js", "~/Scripts/popper.min.js",
+                      "~/Scripts/bootstrap.min.js", 
+                      "~/Scripts/popper.min.js", 
+                      "~/Scripts/feather.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
